f895ddab7cc208fda460745f8ec50d2dcf283568
Samuel ( NAZU ) <108541991+mrnazu@users.noreply.github.com>
2024-01-06 06:23:25 -0800

    mvc-refactor

---
+++ controllers/products.js
@@ -0,0 +1,28 @@
+const products = [];
+
+exports.getAddProduct = (req, res, next) => {
+    res.render('add-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/add-product',
+      formsCSS: true,
+      productCSS: true,
+      activeAddProduct: true
+    });
+};
+
+exports.postAddProducts = (req, res, next) => {
+    products.push({ title: req.body.title });
+    res.redirect('/');
+};
+
+exports.getProduct = (req, res, next) => {
+    const products = adminData.products;
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
+};
